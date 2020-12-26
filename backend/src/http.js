cda7817fc40662c2644a31518a8a97a2198bf512
Kyle Tomanelli <kyletomanelli@gmail.com>
2020-12-26 16:30:44 -0500

    updated get route to return rawJson array from airtable

--- backend/src/http.js
+++ backend/src/http.js
@@ -1,24 +1,28 @@
 const express = require('express');
+const cors = require('cors');
 const { getOpenRoles } = require('./airtable');
-
 
 // Web server config
 const port = 3000;
 const app = express();
 
+app.get(
+  '/api/getOpen',
+  cors({ origin: 'http://localhost:8000' }),
+  async (req, res) => {
+    const data = await getOpenRoles();
+    const rawJson = data.map(item => item._rawJson);
+    res.json(rawJson);
+  }
+);
 
-app.get('/api/getOpen', async (req, res) => {
-  res.json(await getOpenRoles());
-});
-
-//handles 404s
-app.use(function (req, res, next) {
+// handles 404s
+app.use(function(req, res, next) {
   const err = new Error('Not found.');
   err.status = 404;
   next(err);
 });
 
-
-app.listen(port, ()=>{
+app.listen(port, () => {
   console.log(`server is listening on port ${port}`);
 });
