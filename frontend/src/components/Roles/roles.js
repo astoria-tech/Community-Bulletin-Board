b7c15fbd6378ec49e0cf358f5246a129058fd142
Kyle Tomanelli <kyletomanelli@gmail.com>
2020-12-27 14:36:59 -0500

    updated Dockerfile+docker-compose to run build + serve rather then develop. added metadata to gatsby-config, removed un-used packages from roles.js

--- frontend/src/components/Roles/roles.js
+++ frontend/src/components/Roles/roles.js
@@ -1,6 +1,6 @@
 import React,{useState,useEffect} from "react"
 import axios from 'axios'
-import { useStaticQuery, graphql } from "gatsby"
+// import { useStaticQuery, graphql } from "gatsby"
 import SingleRole from "./SingleRole/singleRole"
 import classes from "./roles.module.css"
 
