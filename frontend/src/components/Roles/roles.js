d6e0e186d51f6b90c2576f009a3660b8570adfa0
Kyle Tomanelli <kyletomanelli@gmail.com>
2020-12-28 11:47:48 -0500

    removed localhost:port from axios call

--- frontend/src/components/Roles/roles.js
+++ frontend/src/components/Roles/roles.js
@@ -37,7 +37,7 @@ const Roles = () => {
 
   useEffect(() => {
     axios
-      .get("http://localhost:3000/api/getOpen")
+      .get("/api/getOpen")
       .then(res => setRoles(res.data))
   }, [])
   return (
