b0bec1cb55c06a1d6f1fe8e18568d54e4d7ea1ab
Kyle Tomanelli <kyletomanelli@gmail.com>
2020-12-28 11:31:34 -0500

    added backend proxy object to gatsby-config

--- frontend/gatsby-config.js
+++ frontend/gatsby-config.js
@@ -31,4 +31,8 @@ module.exports = {
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
   ],
+  proxy: {
+    prefix: "/api",
+    url: "http://backend:3000",
+  },
 }
